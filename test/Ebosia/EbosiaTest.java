ddb26f4230e501ce3d5770dfdfdf737e546967d4
mahdiarn <diarn_7@live.com>
2017-03-24 19:35:07 +0700

    Menambah kelas Ebosia

---
+++ test/Ebosia/EbosiaTest.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ebosia;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mahdiar Naufal
+ */
+public class EbosiaTest {
+    
+    public EbosiaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    /**
+     * Testing Default parameter, of class Ebosia.
+     */
+    public void testDefault() {
+        System.out.println("Test Default Parameter");
+        Ebosia instance;
+        instance = new Ebosia ();
+        assertTrue(instance.getWeight() == 0);
+        assertTrue(instance.getTipe() == 0);
+        assertTrue(instance.getX() == -1);
+        assertTrue(instance.getY() == -1);
+    }
+
+    @Test
+    /**
+     * Testing parameter, of class Ebosia.
+     */
+    public void testParameter() {
+        System.out.println("Test Parameter");
+        Ebosia instance;
+        instance = new Ebosia(100,1,true,10,5);
+        assertTrue(instance.getWeight() == 100);
+        assertTrue(instance.getTipe() == 1);
+        assertTrue(instance.getX() == 10);
+        assertTrue(instance.getY() == 5);
+    }
+    
+    @Test
+    /**
+     * Test copy constructor
+     *
+     */
+    public void testCopyConstructor() {
+        System.out.println("Test Copy constructor");
+        Ebosia instance1,instance2;
+        instance1 = new Ebosia(100,1,true,10,5);
+        instance2 = new Ebosia(instance1);
+        assertTrue(instance2.getWeight() == 100);
+        assertTrue(instance2.getTipe() == 1);
+        assertTrue(instance2.getX() == 10);
+        assertTrue(instance2.getY() == 5);
+    }
+    
+    @Test
+    /**
+     * Testing setter, of class Ebosia.
+     */
+    public void testSetter() {
+        System.out.println("Test Setter & Getter");
+        Ebosia instance;
+        instance = new Ebosia();
+        instance.setWeight(200);
+        instance.setTipe(2);
+        instance.setX(100);
+        instance.setY(1000);
+        assertTrue(instance.getWeight() == 200);
+        assertTrue(instance.getTipe() == 2);
+        assertTrue(instance.getX() == 100);
+        assertTrue(instance.getY() == 1000);
+    }
+}
